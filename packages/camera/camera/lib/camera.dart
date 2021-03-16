bedefc6b11d677f3633a5e9d07964897806a2955
Ammar Hemani <ammarhemani@gmail.com>
2021-03-16 00:15:15 -0500

    Export CompressMode enum

--- packages/camera/camera/lib/camera.dart
+++ packages/camera/camera/lib/camera.dart
@@ -11,6 +11,7 @@ export 'package:camera_platform_interface/camera_platform_interface.dart'
         CameraDescription,
         CameraException,
         CameraLensDirection,
+        CompressMode,
         FlashMode,
         ExposureMode,
         WhiteBalanceMode,
